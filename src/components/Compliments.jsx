d4902f4e6c6ebb6833f521f46d95800f1e5b4c67
Jazz7262 <116513312+Jazz7262@users.noreply.github.com>
2024-05-09 11:45:10 +0530

    First Commit

---
+++ src/components/Compliments.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Compliments.css";
+
+function Compliments() {
+  return (
+    <section id="compliments">
+      <h1>With Best Compliments From:</h1>
+      <h5>Mohammed Shahbaz, Chinese Foot Wear,</h5>
+      <h5>Mohammed Mohsin,</h5>
+      <h5>Mohammed Moin,</h5>
+      <h5>Near & Dear - Relatives & Friends.</h5>
+    </section>
+  );
+}
+
+export default Compliments;
